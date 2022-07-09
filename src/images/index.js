a5458fdb21f87bddb73588311825d2e008dc2722
Tilak Puli <tilakpuli15@gmail.com>
2022-07-09 11:42:10 +0530

    demo

---
+++ src/images/index.js
@@ -0,0 +1,11 @@
+import rock from "./rock.png";
+import paper from "./paper.png";
+import scissors from "./scissors.png";
+
+const images = {
+    rock: rock,
+    paper: paper,
+    scissors: scissors,
+};
+
+export default images;
--- src/main/main.js
+++ src/main/main.js
@@ -1,100 +1,103 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
-import robot from "../images/robot.gif"
-import paper from "../images/paper.png"
-import rock from "../images/rock.png"
-import scissors from "../images/scissors.png"
-
-import * as tf from "@tensorflow/tfjs";
+import robot from "../images/robot.gif";
+import * as tf from "@tensorflow/tfjs-core";
+import "@tensorflow/tfjs-backend-webgl";
 import * as handpose from "@tensorflow-models/handpose";
-import { GestureEstimator } from 'fingerpose';
+import { GestureEstimator } from "fingerpose";
+import images from "../images"
 
-import {drawHand} from "./util";
-import {PaperGesture, RockGesture, ScissorsGesture} from "./Gestures";
+import { drawHand } from "./util";
+import { PaperGesture, RockGesture, ScissorsGesture } from "./Gestures";
 
 function Main() {
-    const webcamRef = useRef(null);
-    const canvasRef = useRef(null);
-    const [computerMove, setComputerMove] = useState(null);
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [computerMove, setComputerMove] = useState(null);
 
-    const images = {
-        rock: rock,
-        paper: paper,
-        scissors: scissors
+  const runHandpose = async () => {
+    setInterval(() => {
+      detect();
+    }, 10);
+  };
+
+  const detect = async (net) => {
+    if (isWebCamReady(webcamRef)) {
+      const frame = webcamRef.current.video;
+
+      setCanvasResolution(webcamRef, canvasRef);
     }
+  };
 
-    const runHandpose = async () => {
-        setInterval(() => {
-            detect();
-        }, 10);
-    };
+  useEffect(() => {
+    runHandpose();
+  }, []);
 
-    const detect = async (net) => {
-        if (
-            typeof webcamRef.current !== "undefined" &&
-            webcamRef.current !== null &&
-            webcamRef.current.video.readyState === 4
-        ) {
-            const video = webcamRef.current.video;
-            const videoWidth = webcamRef.current.video.videoWidth;
-            const videoHeight = webcamRef.current.video.videoHeight;
+  return (
+    <div className="App-header">
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "100px",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
 
-            // Set canvas height and width
-            canvasRef.current.width = videoWidth;
-            canvasRef.current.height = videoHeight;
-        }
-    };
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: "absolute",
+          marginLeft: "100px",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
 
-    useEffect( () => {
-        runHandpose()
-    }, [])
+      <img
+        src={images[computerMove] || robot}
+        style={{
+          position: "absolute",
+          marginLeft: "100px",
+          left: 750,
+          background: "red",
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 400,
+          height: 480,
+        }}
+      />
+    </div>
+  );
+}
 
+
+function setCanvasResolution(webcamRef, canvasRef) {
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    // Set canvas height and width
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+}
+
+function isWebCamReady(webcamRef) {
     return (
-            <div className="App-header">
-                <Webcam
-                    ref={webcamRef}
-                    style={{
-                        position: "absolute",
-                        marginLeft: "100px",
-                        left: 0,
-                        right: 0,
-                        textAlign: "center",
-                        zindex: 9,
-                        width: 640,
-                        height: 480,
-                    }}
-                />
-
-                <canvas
-                    ref={canvasRef}
-                    style={{
-                        position: "absolute",
-                        marginLeft: "100px",
-                        left: 0,
-                        right: 0,
-                        textAlign: "center",
-                        zindex: 9,
-                        width: 640,
-                        height: 480,
-                    }}
-                />
-
-                <img
-                    src={images[computerMove] || robot}
-                    style={{
-                        position: "absolute",
-                        marginLeft: "100px",
-                        left: 750,
-                        background: "red",
-                        right: 0,
-                        textAlign: "center",
-                        zindex: 9,
-                        width: 400,
-                        height: 480,
-                    }}
-                />
-            </div>
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
     );
 }
+
 
 export default Main;
